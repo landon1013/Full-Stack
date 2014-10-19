9e047dd75220b7c75213324c9065b8df7d15c771
Landon1013 <landon@thecallfamily.com>
2014-10-19 03:17:37 -0600

    updates all input fields and functionality

--- js/myCtrl.js
+++ js/myCtrl.js
@@ -8,11 +8,37 @@ app.controller('mainCtrl', function($scope, mainService){
         });
     };
 
+    $scope.addFriends = function() {
+        mainService.addFriends($scope.newFriend);
+
+        mainService.getFriends().then(function(data) {
+            $scope.friends = data.data;
+            console.log($scope.friends);
+        });
+
+        $scope.newFriend.name = "";
+        $scope.newFriend.age = "";
+        $scope.newFriend.met = "";
+    };
+
     $scope.getFamily = function() {
         mainService.getFamily().then(function(data) {
             $scope.family = data.data;
             console.log($scope.family);
         });
+    };
+
+    $scope.addFamilyMember =function() {
+        mainService.addFamilyMember($scope.newFamilyMember);
+
+        mainService.getFamily().then(function(data) {
+            $scope.family = data.data;
+            console.log($scope.family);
+        });
+
+        $scope.newFamilyMember.name = "";
+        $scope.newFamilyMember.age = "";
+        $scope.newFamilyMember.relationship = "";
     };
 
     $scope.getActivities = function() {
@@ -22,23 +48,21 @@ app.controller('mainCtrl', function($scope, mainService){
         });
     };
 
+    $scope.addActivity = function() {
+        mainService.addActivity($scope.newActivity)
+
+        mainService.getActivities().then(function(data) {
+            $scope.activities = data.data;
+            console.log($scope.activities);
+        });
+
+        $scope.newActivity.name = "";
+    }
+
     $scope.getMe = function() {
         mainService.getMe().then(function(data) {
             $scope.me = data.data;
             console.log($scope.me);
         });
     };
-
-    $scope.addFriends = function() {
-        mainService.addFriends($scope.newFriend);
-
-        mainService.getFriends().then(function(data) {
-            $scope.friends = data.data;
-            console.log($scope.friends);
-        });
-
-        $scope.addFriendName = "";
-        $scope.addFriendAge = "";
-        $scope.addFriendMet = "";
-    }
 });
--- js/myService.js
+++ js/myService.js
@@ -27,11 +27,33 @@ app.service("mainService", function($http) {
         });
     };
 
+    this.addFamilyMember = function(newFamilyMember) {
+        return $http({
+            method: "POST",
+            url: "http://localhost:3000/family/new",
+            data: {
+                name: newFamilyMember.name,
+                age: newFamilyMember.age,
+                relationship: newFamilyMember.relationship
+            }
+        })
+    };
+
     this.getActivities = function() {
         return $http({
             method: "GET",
             url: "http://localhost:3000/activities"
         });
+    };
+
+    this.addActivity = function(newActivity) {
+        return $http({
+            method: "POST",
+            url: "http://localhost:3000/activities/new",
+            data: {
+                name: newActivity.name
+            }
+        })
     };
 
     this.getMe = function() {
